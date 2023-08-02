1f07aaa287c63eb99dddd59fea3bfd94a49908aa
albertojimenez <jimenezbaezalberto@gmail.com>
2023-08-02 19:31:30 +0200

    cambiando viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react';
 
 
 // https://vitejs.dev/config/
-export default defineConfig(
+export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://ytpa6cbcqt.eu-west-1.awsapprunner.com/",
-      "/auth": "https://ytpa6cbcqt.eu-west-1.awsapprunner.com/"
+      '/api': {
+        target: 'https://ytpa6cbcqt.eu-west-1.awsapprunner.com',
+        changeOrigin: true, // Cambia el origen de la solicitud al objetivo del proxy
       },
-      
-    // hmr: {
-    //   host: 'localhost'
-    //     }
-    // }
+      '/auth': {
+        target: 'https://ytpa6cbcqt.eu-west-1.awsapprunner.com',
+        changeOrigin: true, 
+      },
+    },
     }
-
-)
+);
